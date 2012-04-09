d1da4b3075f77d3124665b750b2fd6ac22f6f463
Beguene Permale <beguene.permale@gmail.com>
2012-04-09 11:19:33 +0530

    add script to generate ctags

---
+++ scripts/ctagPHP.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~/htdocs/Symfony/src
+exec ctags -f ../tags \
+    -h \".php\" -R \
+    --exclude=\"\.svn,\.git\" \
+    --totals=yes \
+    --tag-relative=yes \
+    --PHP-kinds=+cf \
+    --regex-PHP='/abstract class ([^ ]*)/\1/c/' \
+    --regex-PHP='/interface ([^ ]*)/\1/c/' \
+    --regex-PHP='/(public |static |abstract |protected |private )+function ([^
+(]*)/\2/f/''])'
